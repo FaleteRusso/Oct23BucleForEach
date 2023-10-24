5cd75ac95348ea54195257df31cc825201c757fd
FaleteRusso <faleterusso89@gmail.com>
2023-10-24 19:05:28 +0200

    Initial commit

---
+++ src/Ejercicio18.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class EjercicioAyer {
+    public static void main(String[] args) {
+        //leer una frase y contar cuantas vocales tiene
+        String frase= "";
+        Scanner teclado = new Scanner(System.in);
+        int contadorVocales =0;
+        System.out.println("introduzca una frase");
+        frase=teclado.nextLine();
+        for (int i = 0; i< frase.length();i++){
+            switch (frase.charAt(i)){
+                case 'a','i','o','u','A','E','I','O','U':
+                    contadorVocales++;
+                    break;
+
+            }
+        }
+        System.out.println("La frase tiene " + contadorVocales + " vocales");
+    }
+}
